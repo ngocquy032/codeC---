be70f29fab0cdcef0a3cfc487cc65a5af88b70d6
ngocquy032 <112050702+ngocquy032@users.noreply.github.com>
2023-05-15 13:34:35 +0700

    example1

---
+++ Atom.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex1
+{
+    internal class Atom
+    {
+        private int atomicNumber;
+        private string symbol;
+        private string fullName;
+        private double atomicWeight;
+
+        public bool accept()
+        {
+            Console.Write("Nhập số nguyên tử: ");
+            string input = Console.ReadLine();
+            if (!int.TryParse(input, out atomicNumber))
+            {
+                Console.WriteLine("Dữ liệu không hợp lệ. Vui lòng nhập một số nguyên hợp lệ cho số nguyên tử.");
+                return false;
+            }
+
+            Console.Write("Nhập ký hiệu: ");
+            symbol = Console.ReadLine();
+
+            Console.Write("Nhập tên đầy đủ: ");
+            fullName = Console.ReadLine();
+
+            Console.Write("Nhập khối lượng nguyên tử: ");
+            input = Console.ReadLine();
+            if (!double.TryParse(input, out atomicWeight))
+            {
+                Console.WriteLine("Dữ liệu không hợp lệ. Vui lòng nhập một số thực hợp lệ cho khối lượng nguyên tử.");
+                return false;
+            }
+
+            return true;
+        }
+
+        public void display()
+        {
+            Console.WriteLine($"{atomicNumber,-3} {symbol,-3} {fullName,-10} {atomicWeight}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Atom[] atoms = new Atom[3];
+            int count = 0;
+
+            Console.WriteLine("Thông tin nguyên tử");
+            Console.WriteLine("==================");
+
+            while (count < atoms.Length)
+            {
+                atoms[count] = new Atom();
+               if (!atoms[count].accept())
+                {
+                    continue;
+               }
+
+                count++;
+
+                if (count == atoms.Length)
+                {
+                    break;
+                }
+
+            }
+
+            Console.WriteLine("No Sym Name       Weight");
+            Console.WriteLine("------------------------------------");
+            for (int i = 0; i < count; i++)
+            {
+                atoms[i].display();
+            }
+        }
+    }
+}
